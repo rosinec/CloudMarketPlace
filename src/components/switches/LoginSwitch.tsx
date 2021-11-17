0a8c4dd0ba5163c43172d5a459f6158d4c2be714
Matěj Novotný <485457@muni.cz>
2021-11-17 21:16:47 +0100

    Remove tooltip cause of warnings

--- src/components/switches/LoginSwitch.tsx
+++ src/components/switches/LoginSwitch.tsx
@@ -1,18 +1,17 @@
 import { FC } from 'react';
-import { IconButton, Tooltip } from '@mui/material';
+import { IconButton } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Link } from 'react-router-dom';
 
-import { useTranslation } from '../../hooks/useTranslation';
 import { useLoggedInUser } from '../../hooks/useLoggedInUser';
 import { signOut } from '../../utils/firebase';
 
 const LoginSwitch: FC = () => {
 	const user = useLoggedInUser();
-	const t = useTranslation();
 	return (
-		<Tooltip title={!user ? t('layout.login') : t('layout.logout')}>
+		// eslint-disable-next-line react/jsx-no-useless-fragment
+		<>
 			{!user ? (
 				<IconButton component={Link} to="/login">
 					<LoginIcon fontSize="inherit" />
@@ -22,7 +21,7 @@ const LoginSwitch: FC = () => {
 					<LogoutIcon fontSize="inherit" />
 				</IconButton>
 			)}
-		</Tooltip>
+		</>
 	);
 };
 
