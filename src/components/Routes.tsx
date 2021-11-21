2220cc430daf2473c05a8fe9434712d2d8bc9371
Matěj Novotný <485457@muni.cz>
2021-11-21 18:52:52 +0100

    Introduce AuthenticatedRoute

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,16 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
+import { FC } from 'react';
+import { Switch, Route, Redirect, RouteProps } from 'react-router-dom';
 
 import { useLoggedInUser } from '../hooks/useLoggedInUser';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 
+type Props = {
+	C: () => JSX.Element;
+	isLogged: boolean;
+} & RouteProps;
+
+const AuthenticatedRoute: FC<Props> = ({ C, isLogged, ...rest }) => (
+	<Route
+		{...rest}
+		render={() => (isLogged ? <C /> : <Redirect to="/login" />)}
+	/>
+);
+
 const Routes = () => {
 	const user = useLoggedInUser();
+	const isLogged = user !== null;
 	return (
 		<Switch>
-			<Route path="/" exact component={Home} />
 			{!user && <Route path="/login" exact component={Login} />}
+			<AuthenticatedRoute C={Home} isLogged={isLogged} path="/" />
+
 			<Route component={NotFound} />
 		</Switch>
 	);
