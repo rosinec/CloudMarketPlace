9f6fb3533a77ecc726af3c76c6d3eee39f53016a
Adrian Rosinec <adrian@ics.muni.cz>
2021-12-09 10:14:30 +0100

    Add: Featured to all.

--- src/pages/All.tsx
+++ src/pages/All.tsx
@@ -1,9 +1,10 @@
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
 
 import { useTranslation } from '../hooks/useTranslation';
 import usePageTitle from '../hooks/usePageTitle';
 import AppCard from '../components/AppCard';
 import { useFilterDrawer } from '../hooks/useFilterDrawer';
+import AppFeatured from '../components/AppFeatured';
 
 const All = () => {
 	const t = useTranslation();
@@ -11,16 +12,21 @@ const All = () => {
 
 	const [, , apps] = useFilterDrawer();
 
+	const featured = apps.filter(app => app.featured === true)[0];
 	return (
-		<Box>
-			<Grid container spacing={2}>
-				{apps.map((app, index) => (
-					<Grid key={index} item xs={12} sm={6} md={4}>
-						<AppCard {...app} />
-					</Grid>
-				))}
-			</Grid>
-		</Box>
+		<>
+			{featured ? <AppFeatured {...featured} /> : console.log(featured)}
+			<Typography variant="h4">Available for you</Typography>
+			<Box>
+				<Grid container spacing={2}>
+					{apps.map((app, index) => (
+						<Grid key={index} item xs={12} sm={6} md={4}>
+							<AppCard {...app} />
+						</Grid>
+					))}
+				</Grid>
+			</Box>
+		</>
 	);
 };
 
